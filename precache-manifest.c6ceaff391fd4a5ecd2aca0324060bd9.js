41e91941785169d5166f794017cceda7e66c8f27
booth-algo <kvl01@protonmail.com>
2024-06-26 03:25:58 +0100

    Updates

---
+++ precache-manifest.c6ceaff391fd4a5ecd2aca0324060bd9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81db1049831192f878ce2887c095c690",
+    "url": "/blog/index.html"
+  },
+  {
+    "revision": "0d7482a856d9ff6acdda",
+    "url": "/blog/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "a023ad839e7909cfd73f",
+    "url": "/blog/static/js/2.5d347d43.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/blog/static/js/2.5d347d43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7ef94c7dd6da82ecbd0",
+    "url": "/blog/static/js/3.e637123c.chunk.js"
+  },
+  {
+    "revision": "0d7482a856d9ff6acdda",
+    "url": "/blog/static/js/main.eae592e2.chunk.js"
+  },
+  {
+    "revision": "824c3ad09ae2a530b854",
+    "url": "/blog/static/js/runtime-main.712954f1.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/blog/static/media/logo.06e73328.svg"
+  }
+]);
